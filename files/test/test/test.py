78c6f420e0f9f8b6b773e38fc3b7400aa608cdd0
Andres Barrera <drebarrera@yahoo.com>
2022-02-13 22:10:44 -0500

    Modular Updates

--- files/test/test/test.py
+++ files/test/test/test.py
@@ -6,9 +6,20 @@ sys.path.append(homedir)
 data = mx.Data()
 body = mx.Body()
 
+
 ### OBJECTS ###
-data.title = "hi"
-print(data.charset)
+text = mx.T()
+text2 = mx.T()
+t = mx.Table()
+m = mx.Menu()
+i = mx.Icon()
+i.type = "x"
 ### CONTENT ###
+body.content = [t]
+text.content = "Text"
+text2.content = "Text2"
+t.content = [[m,i,text],[text,text,text]]
 
 ### PROPERTIES ###
+m.spacing = "1"
+i.height = "20"
